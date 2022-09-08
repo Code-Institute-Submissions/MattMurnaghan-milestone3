ec3c86df6ec57adc71dbb0719773386459fd433c
Matthew Murnaghan <matt.murnaghan@outlook.ie>
2022-09-08 06:53:56 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import plotext as pt
+
+x_vals = [1,2,3,3,4,5,6,7,8]
+y_vals = [1,2,3,3,4,5,6,7,8]
+
+pt.bar(x_vals,y_vals)
+pt.title('TEST')
+pt.show()
