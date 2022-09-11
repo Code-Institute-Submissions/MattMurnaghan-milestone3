68d58f0c5afd7ffcfa82c92a8922fc108a30d8de
Matthew Murnaghan <matt.murnaghan@outlook.ie>
2022-09-11 13:23:15 +0000

    Add requirements for heroku

--- run.py
+++ run.py
@@ -4,9 +4,9 @@
 
 import plotext as pt
 
-x_vals = [1,2,3,3,4,5,6,7,8]
-y_vals = [1,2,3,3,4,5,6,7,8]
+y_vals = [1, 2, 3, 3, 4, 5, 6, 7, 8]
+x_vals = [1, 2, 3, 3, 4, 5, 6, 7, 8]
 
-pt.bar(x_vals,y_vals)
+pt.bar(x_vals, y_vals)
 pt.title('TEST')
 pt.show()
